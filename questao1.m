2a474657b164e062099b0e4dbc6973fb70d97b03
Bernardo Bevilaqua <bernardo.bfg@poli.ufrj.br>
2023-07-16 15:29:29 -0300

    Update questao1.m

--- questao1.m
+++ questao1.m
@@ -1,9 +1,8 @@
 % Carregar o sinal de voz
-[voz, fs_voz] = audioread('voz.mpeg'); % Substitua 'voz.mpeg' pelo nome do arquivo que contém o sinal de voz
+[voz, fs_voz] = audioread('voz.mpeg');
 
 % Carregar o sinal de música no formato .mpeg
-[musica, fs_musica] = audioread('musica.mpeg'); % Substitua 'musica.mpeg' pelo nome do arquivo que contém o sinal de música
-
+[musica, fs_musica] = audioread('musica.mpeg');
 % Frequência de amostragem desejada
 fs_target = 16000;
 
@@ -30,22 +29,14 @@ window_size = round(0.03 * fs_target); % Tamanho da janela (aproximadamente 30 m
 overlap = round(window_size * 0.5); % Sobreposição entre janelas (50%)
 nfft = 2^nextpow2(window_size); % Tamanho da FFT (potência de 2)
 
-% Calcular o eixo x em número de amostras
-num_amostras = size(voz_resampled, 1);
-eixo_x = (0:num_amostras-1)';
-
 % Espectrograma do sinal de voz reamostrado
 figure;
 spectrogram(voz_resampled, hamming(window_size), overlap, nfft, fs_target, 'yaxis');
-set(gca, 'XTick', linspace(0, num_amostras, 5), 'XTickLabel', linspace(0, num_amostras, 5));
-xlabel('Número de Amostras');
 title('Espectrograma do Sinal de Voz (Reamostrado)');
 colorbar;
 
 % Espectrograma do sinal de música reamostrado
 figure;
 spectrogram(musica_resampled, hamming(window_size), overlap, nfft, fs_target, 'yaxis');
-set(gca, 'XTick', linspace(0, num_amostras, 5), 'XTickLabel', linspace(0, num_amostras, 5));
-xlabel('Número de Amostras');
 title('Espectrograma do Sinal de Música (Reamostrado)');
 colorbar;
